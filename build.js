ff29b05f99a1ba7b28594dc60228b6fdb30878d5
Miller <miller.yu@leandev.se>
2017-12-19 18:26:19 +0800

    add style file

--- build.js
+++ build.js
@@ -117,6 +117,7 @@ return Promise.resolve()
   })
   // Copy package files
   .then(() => Promise.resolve()
+    .then(() => _relativeCopy('**/*.css', tempLibFolder, distFolder))
     .then(() => _relativeCopy('LICENSE', rootFolder, distFolder))
     .then(() => _relativeCopy('package.json', rootFolder, distFolder))
     .then(() => _relativeCopy('README.md', rootFolder, distFolder))
