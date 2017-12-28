515a96ab8f5d5418a7d176292dac0a7d7013521e
Miller <miller.yu@leandev.se>
2017-12-28 10:48:21 +0800

    remove error ExpressionChangedAfterItHasBeenCheckedError

--- integration/src/app/service/template.service.ts
+++ integration/src/app/service/template.service.ts
@@ -1,4 +1,4 @@
-import {Injectable, TemplateRef} from '@angular/core';
+import {ChangeDetectorRef, Component, Inject, Injectable, TemplateRef} from '@angular/core';
 import {OPTIONS_TEMPLATE} from './options-template';
 
 @Injectable()
@@ -6,44 +6,74 @@ export class TemplateService {
 
     private codeCache = {
         'default': `@Component({
-  selector: 'default-busy',
-  template: \`
-    <div class="ng-busy-default-wrapper">
-      <div class="ng-busy-default-sign">
-        <div class="ng-busy-default-spinner">
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
-          <div class="bar4"></div>
-          <div class="bar5"></div>
-          <div class="bar6"></div>
-          <div class="bar7"></div>
-          <div class="bar8"></div>
-          <div class="bar9"></div>
-          <div class="bar10"></div>
-          <div class="bar11"></div>
-          <div class="bar12"></div>
+    selector: 'default-busy',
+    template: \`
+        <div class="ng-busy-default-wrapper">
+            <div class="ng-busy-default-sign">
+                <div class="ng-busy-default-spinner">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                    <div class="bar4"></div>
+                    <div class="bar5"></div>
+                    <div class="bar6"></div>
+                    <div class="bar7"></div>
+                    <div class="bar8"></div>
+                    <div class="bar9"></div>
+                    <div class="bar10"></div>
+                    <div class="bar11"></div>
+                    <div class="bar12"></div>
+                </div>
+                <div class="ng-busy-default-text">{{message}}</div>
+            </div>
         </div>
-        <div class="ng-busy-default-text">{{message}}</div>
-      </div>
-    </div>
-  \`,
+    \`,
 })
 export class DefaultBusyComponent {
-  constructor(@Inject('message')public message: string) {};
+    private _msg: string;
+
+    constructor(@Inject('message') private msg: string, private _changeDetectionRef: ChangeDetectorRef) {
+    }
+
+    get message() {
+        if (this._msg === undefined) {
+            this.message = this.msg;
+        }
+        return this._msg;
+    }
+
+    set message(msg: string) {
+        this._msg = msg;
+        this._changeDetectionRef.detectChanges();
+    }
 }`,
         'custom': `@Component({
-  selector: 'default-busy',
-  template: \`
-    <div style="background: url('asset/img/du.gif') no-repeat center 20px; background-size: 72px;">
-      <div style="margin-top: 110px; text-align: center; font-size: 18px; font-weight: 700; line-height: 110px;">
-        {{message}}
-      </div>
-    </div>
-  \`,
+    selector: 'default-busy',
+    template: \`
+        <div style="background: url('asset/img/du.gif') no-repeat center 20px; background-size: 72px;">
+            <div style="margin-top: 110px; text-align: center; font-size: 18px; font-weight: 700; line-height: 110px;">
+                {{message}}
+            </div>
+        </div>
+    \`,
 })
 export class CustomBusyComponent {
-  constructor(@Inject('message')public message: string) {};
+    private _msg: string;
+
+    constructor(@Inject('message') private msg: string, private _changeDetectionRef: ChangeDetectorRef) {
+    };
+
+    get message() {
+        if (this._msg === undefined) {
+            this.message = this.msg;
+        }
+        return this._msg;
+    }
+
+    set message(msg: string) {
+        this._msg = msg;
+        this._changeDetectionRef.detectChanges();
+    }
 }`,
         'template': `<ng-template #customTemplate>
     <div style="margin-top: 110px; text-align: center;">Hi, This is from ng-template.</div>
--- src/lib/src/model/busy-config.ts
+++ src/lib/src/model/busy-config.ts
@@ -1,5 +1,5 @@
 import {Subscription} from 'rxjs/Subscription';
-import {Component, TemplateRef, Type, Inject} from '@angular/core';
+import {Component, TemplateRef, Type, Inject, ChangeDetectorRef} from '@angular/core';
 
 export class BusyConfig implements IBusyConfig {
     template: TemplateRef<any> | Type<any>;
@@ -17,31 +17,46 @@ export class BusyConfig implements IBusyConfig {
 }
 
 @Component({
-  selector: 'default-busy',
-  template: `
-    <div class="ng-busy-default-wrapper">
-      <div class="ng-busy-default-sign">
-        <div class="ng-busy-default-spinner">
-          <div class="bar1"></div>
-          <div class="bar2"></div>
-          <div class="bar3"></div>
-          <div class="bar4"></div>
-          <div class="bar5"></div>
-          <div class="bar6"></div>
-          <div class="bar7"></div>
-          <div class="bar8"></div>
-          <div class="bar9"></div>
-          <div class="bar10"></div>
-          <div class="bar11"></div>
-          <div class="bar12"></div>
+    selector: 'default-busy',
+    template: `
+        <div class="ng-busy-default-wrapper">
+            <div class="ng-busy-default-sign">
+                <div class="ng-busy-default-spinner">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                    <div class="bar3"></div>
+                    <div class="bar4"></div>
+                    <div class="bar5"></div>
+                    <div class="bar6"></div>
+                    <div class="bar7"></div>
+                    <div class="bar8"></div>
+                    <div class="bar9"></div>
+                    <div class="bar10"></div>
+                    <div class="bar11"></div>
+                    <div class="bar12"></div>
+                </div>
+                <div class="ng-busy-default-text">{{message}}</div>
+            </div>
         </div>
-        <div class="ng-busy-default-text">{{message}}</div>
-      </div>
-    </div>
-  `,
+    `,
 })
 export class DefaultBusyComponent {
-  constructor(@Inject('message')public message: string) {};
+    private _msg: string;
+
+    constructor(@Inject('message') private msg: string, private _changeDetectionRef: ChangeDetectorRef) {
+    }
+
+    get message() {
+        if (this._msg === undefined) {
+            this.message = this.msg;
+        }
+        return this._msg;
+    }
+
+    set message(msg: string) {
+        this._msg = msg;
+        this._changeDetectionRef.detectChanges();
+    }
 }
 
 export interface IBusyConfig {
